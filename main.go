7d838e2741a97723fc123734b7bd33c5e9702d6c
smlb <smlbr01@gmail.com>
2017-10-09 21:00:03 +0200

    Peroni my love

--- main.go
+++ main.go
@@ -36,6 +36,8 @@ const (
 	zero   = "\x30\xE2\x83\xA3"
 	dexcl  = "\xE2\x80\xBC"
 	exclq  = "\xE2\x81\x89"
+	beer   = "\xF0\x9F\x8D\xBA"
+	dbeer  = "\xF0\x9F\x8D\xBB"
 	terr   = "\xF0\x9F\x91\xB3 \xF0\x9F\x92\xA3"
 	ngul   = "\xF0\x9F\x86\x96 \xF0\x9F\x86\x92"
 	suh    = "suh my ni\xF0\x9F\x85\xB1\xF0\x9F\x85\xB1a"
@@ -118,6 +120,8 @@ func setupReplacer() {
 	//
 	// Will rewrite this shitty thingy later.
 	replacer = *strings.NewReplacer(
+		"2PERONI", dbeer,
+		"PERONI", beer,
 		"SOS", sos,
 		"AB", ab,
 		"CL", cl,
