cf991803e1bd03f6b6fc6dac5f5f289922eb653c
Gianguido Sora <g.sora4@gmail.com>
2017-10-10 18:24:48 +0200

    handle for real, now

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
-	"github.com/gsora/nibberbot/breath"
 	"github.com/gsora/nibberbot/nibber"
 )
 
@@ -52,43 +51,8 @@ func main() {
 	updates := bot.ListenForWebhook("/" + apiKey)
 	go startServer()
 	for update := range updates {
-		if update.InlineQuery != nil {
-			log.Printf("[INLINE] new query sent in by %s -> %s\n", update.InlineQuery.From.UserName, update.InlineQuery.Query)
-			payload := []interface{}{}
-			memeStr := nibberInstance.Nibbering(update.InlineQuery.Query)
-
-			article := tgbotapi.NewInlineQueryResultArticle(update.InlineQuery.ID, suh, memeStr)
-			article.Description = memeStr
-			payload = append(payload, article)
-
-			breathingMemeStr, err := breath.Breath(memeStr)
-			if err != nil {
-				log.Printf("[ERROR] cannot breath for request %s\n", update.InlineQuery.Query)
-			} else {
-				breathingArticle := tgbotapi.NewInlineQueryResultArticle(update.InlineQuery.ID+"-breathing", breathingSuh, breathingMemeStr)
-				breathingArticle.Description = breathingMemeStr
-				payload = append(payload, breathingArticle)
-			}
-
-			inlineConf := tgbotapi.InlineConfig{
-				InlineQueryID: update.InlineQuery.ID,
-				IsPersonal:    true,
-				CacheTime:     0,
-				Results:       payload,
-			}
-
-			if _, err := bot.AnswerInlineQuery(inlineConf); err != nil {
-				log.Println(err)
-			}
-		} else if update.Message != nil {
-			log.Printf("[MESSAGE] new message sent in by %s -> %s\n", update.Message.From.UserName, update.Message.Text)
-			memeStr := nibberInstance.Nibbering(update.Message.Text)
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, memeStr)
-			msg.ReplyToMessageID = update.Message.MessageID
-			if _, err := bot.Send(msg); err != nil {
-				log.Println(err)
-			}
-		}
+		// handle every update in a separate goroutine
+		go handleUpdate(update, bot)
 	}
 
 }
